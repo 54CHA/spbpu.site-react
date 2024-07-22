576ea7616808d9b7ff04c54093a1c1af1a6b7152
54CHA <shsha.khv@gmail.com>
2024-07-22 08:14:33 +0300

    google analytics

--- src/index.js
+++ src/index.js
@@ -5,10 +5,22 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render(  
+  <div>
+    <head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-BF9Q5Q7J2F"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-BF9Q5Q7J2F');
+  </script>
+  </head>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function

4a28e9f32ad11eab00c0ff5e2fd55c22c4128011
54CHA <shsha.khv@gmail.com>
2024-07-23 05:45:50 +0300

    added target blank for link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { faTelegram } from '@fortawesome/free-brands-svg-icons';
 
 
 const Footer = () => {
-    return ( <footer className="footer">Not affiliated with SPBSTU <a href='https://t.me/sachakhv'><FontAwesomeIcon icon={faTelegram} className='faTelegram'/></a>
+    return ( <footer className="footer">Not affiliated with SPBSTU <a href='https://t.me/sachakhv' target='_blank'><FontAwesomeIcon icon={faTelegram} className='faTelegram'/></a>
 </footer> );
 }
  
